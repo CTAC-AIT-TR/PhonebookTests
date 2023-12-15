1639f9ea7dd795acc8f3bfc41f9534ad0b1fed0b
CTAC-AIT-TR <justas@unitybox.de>
2023-12-16 00:14:44 +0100

    rebuild

---
+++ src/main/java/com/ait/phonebook/utils/ContactData.java
@@ -0,0 +1,10 @@
+package com.ait.phonebook.utils;
+
+public class ContactData {
+    public static final String NAME = "Bob";
+    public static final String LAST_NAME = "Schmidt";
+    public static final String PHONE = "1234567890987";
+    public static final String EMAIL = "bob@gmx.de";
+    public static final String ADDRESS = "London";
+    public static final String DESCRIPTION = "student";
+}
---
+++ src/main/java/com/ait/phonebook/utils/UserData.java
@@ -0,0 +1,6 @@
+package com.ait.phonebook.utils;
+
+public class UserData {
+    public static final String EMAIL = "stas@gmx.de";
+    public static final String PASSWORD = "s12345678S$";
+}
